7e254a0721a26b9002ec46a397f7cba844917bc4
Musashi-Sakamoto <1292602b@gmail.com>
2023-01-15 17:19:37 -0800

    update package

--- packages/random-number/lib/random-number.js
+++ packages/random-number/lib/random-number.js
@@ -3,6 +3,6 @@
 module.exports = randomNumber;
 
 function randomNumber(min, max) {
-    const num = Math.floor(Math.random() * (max - min) + min);
+    const num = Math.floor(Math.random() * (max - min + 1) + min);
     num;
 }
